5a8c0c6d9b6bb14e77d6514302f853755282fa96
Marcin Guszpit <marcin.guszpit@gmail.com>
2018-06-21 15:06:08 +0200

    google maps begin2

--- src/app/utilities/google.maps.utils.ts
+++ src/app/utilities/google.maps.utils.ts
@@ -1,16 +1,41 @@
-import {createClient, GoogleMapsClient} from '@google/maps';
+import * as google from '@google/maps';
 import {AppSettings} from '../settings/appSettings';
-import {} from '@google/maps';
 
-const mapsClient: GoogleMapsClient = createClient({
+const mapsClient = google.createClient({
     key: AppSettings.maps.apiKey,
     Promise,
 });
 
 class GoogleMapsUtilsService {
+    private map: google.maps.Map;
+    private marker: google.maps.Marker;
+
     public findLocation(adress: string) {
         console.log('find location');
         return mapsClient.geocode({address: adress}).asPromise();
+    }
+
+    public markLocationOnMap(position: any, element: Element, defZoom: number = 8) {
+        this.map = new google.maps.Map(element, {
+            center: position,
+            zoom: defZoom,
+        });
+        this.marker = new google.maps.Marker({position, map: this.map});
+        console.log('mark location on map');
+    }
+
+    public currentPositionFromGeolocation(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            if (navigator || navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition((position) => {
+                    resolve(position.coords);
+                }, (error) => {
+                    reject(error);
+                });
+            } else {
+                reject(new Error('Navigator.geolocation does not exist'));
+            }
+        });
     }
 }
 
--- test/maps-test/maps.tests.ts
+++ test/maps-test/maps.tests.ts
@@ -2,8 +2,10 @@ import {googleMapsService} from '../../src/app/utilities/google.maps.utils';
 
 const locationStr = <HTMLInputElement> document.querySelector('input[name="location-string"]');
 const locationBtn = document.querySelector('input[name="loc-btn"]');
+const geoLocationBtn = document.querySelector('input[name="geoloc-btn"]');
 
 console.log(locationBtn);
+console.log(geoLocationBtn);
 console.log(locationStr);
 
 locationBtn.addEventListener('click', () => {
@@ -14,6 +16,18 @@ locationBtn.addEventListener('click', () => {
         console.log(value);
     }).catch((error) => {
         console.log('ERROR!!!');
+        console.log(error);
+    });
+});
+
+geoLocationBtn.addEventListener('click', () => {
+    console.log('geolocation position');
+    const elem = document.querySelector('.map-div');
+    elem.innerHTML = '';
+    googleMapsService.currentPositionFromGeolocation().then((position) => {
+        console.log(position);
+        googleMapsService.markLocationOnMap(position, elem);
+    }).catch((error) => {
         console.log(error);
     });
 });
